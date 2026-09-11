08558234c02c036d877b510aa81ae8d2063ffe42
Rizwan <eventandcraft.app@gmail.com>
2026-09-11 15:55:50 +0400

    feat: integrate Google Tag Manager into root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Fira_Sans } from "next/font/google";
 import "./globals.css";
 import { CartProvider } from "@/context/CartContext";
 import Toast from "@/components/Toast";
+import Script from "next/script";
 
 const firaSans = Fira_Sans({
   weight: ["300", "400", "500", "600", "700"],
@@ -94,6 +95,15 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${firaSans.variable}`}>
       <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NQJ2GBHG');
+          `}
+        </Script>
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
@@ -104,6 +114,11 @@ export default function RootLayout({
         />
       </head>
       <body className="bg-surface text-on-surface antialiased selection:bg-primary/20 selection:text-primary min-h-screen flex flex-col">
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: \`<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NQJ2GBHG" height="0" width="0" style="display:none;visibility:hidden"></iframe>\`,
+          }}
+        />
         <CartProvider>
           {children}
           <Toast />
